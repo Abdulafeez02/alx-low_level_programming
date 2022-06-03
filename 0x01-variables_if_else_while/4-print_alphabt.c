d23e802ae5bced8a0fd9abc27b2a2ac4bccc9a31
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-03 14:17:55 -0700

    great

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints alphabet in lower letter except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'q'  && letter != 'e')
+			putchar(letter);
+	putchar('\n');
+	return (0);
+}
