a1b53d05d133f654924286c0ba7ec7f1731265f2
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-03 15:12:00 -0700

    great

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - prints out all single digit starting from 0 , 
+ * main - prints out all single digit starting from 0 ,
  * followed by a new line
  *
  * Return: Always 0 (Success)
@@ -11,7 +11,7 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++ )
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	putchar('\n');
 	return (0);
