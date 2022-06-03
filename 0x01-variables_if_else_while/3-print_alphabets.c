ee49015da99be161752425a3d165179cac7b8bca
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-03 14:04:26 -0700

    great

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Prints alphabet in lower and upper
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	return (0);
+}
