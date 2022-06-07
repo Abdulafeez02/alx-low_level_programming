ccf24d9b8f6e3fe7a2c99322fef779590978146b
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-07 22:11:07 +0100

    great

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints 1 when it discovers an alphabet
+ *
+ */
+
+int _isalpha(int c)
+{
+	char letter;
+	int num;
+
+	for (letter='a'; letter <= 'z'; letter++)
+	{
+		if (letter == c )
+			num = 1;
+	}
+	return (0);
+}
