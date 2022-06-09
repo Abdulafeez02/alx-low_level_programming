778eb37a4adb05bb5072bf53f329325b9c7c570d
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-09 03:52:06 -0700

    "great"

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative -  returns if an integer is positive
+ * or negative
+ * Retrun: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negetive - returns if a number is positive or otherwise
+ */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN _H */
