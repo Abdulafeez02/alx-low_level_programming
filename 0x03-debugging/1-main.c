a91e16a8397281e63426fc5d18f01d3dbd0c98d9
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-09 09:51:16 -0700

    DONE

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - causes an infinite loop
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming:(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	/*while (1 < 10)*/
+	/*while (i < 10)*/
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
-	
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,9 @@
 
 #include <stdio.h>
 
-/**
- * positive_or_negetive - returns if a number is positive or otherwise
- */
-
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
