6cc04f9f70bf20da73967dafeb264ec073f0dcf3
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-09 03:59:03 -0700

    "great"

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * positive_or_negative -  returns if an integer is positive
- * or negative
+ * main - test function that prints if integer is positive or negaative
  * Retrun: Always 0 (Success)
  */
 
