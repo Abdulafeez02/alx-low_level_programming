e29091656c1d603fe19b45eeeadcfd42af3d9271
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-09 05:13:57 -0700

    "great"

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,7 +9,7 @@ void print_numbers(void)
 {
 	int number;
 
-	for (number =1; number <= 9; number ++)
+	for (number = 48; number <= 57; number ++)
 		_putchar(number);
 	_putchar('\n');
 	return (0);
