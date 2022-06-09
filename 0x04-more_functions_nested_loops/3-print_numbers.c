10ebd84c0bf60585564f1c018852ba8b154a9096
Abdulafeez02 <simplicityreal4@gmail.com>
2022-06-09 05:11:23 -0700

    "great"

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: solution
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number =1; number <= 9; number ++)
+		_putchar(number);
+	_putchar('\n');
+	return (0);
+}
